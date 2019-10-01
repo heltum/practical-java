6a5ecb94362a29f49a3393caeb1bc9f5e2741737
Christine VIncenza <christine1997vincenza@gmail.com>
2019-10-01 10:24:59 +0700

    Initial Commit

---
+++ src/main/java/com/example/practicaljava/PracticalJavaApplication.java
@@ -0,0 +1,14 @@
+package com.example.practicaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PracticalJavaApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalJavaApplication.class, args);
+	}
+
+}
