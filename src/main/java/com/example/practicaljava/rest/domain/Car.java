95e9ffe595aeeaf10be9f9a4197404d5a3c75869
Christine Vincenza Sitorus <christine1997vincenza@gmail.com>
2019-10-02 08:23:19 +0700

    Commit Car.Java
    Aadd getter setter to convert JAVA to JSON

--- src/main/java/com/example/practicaljava/rest/domain/Car.java
+++ src/main/java/com/example/practicaljava/rest/domain/Car.java
@@ -1,9 +1,17 @@
 package com.example.practicaljava.rest.domain;
 
+import java.util.Date;
+
 public class Car {
+	private boolean available;
 	private String brand;
 	private String color;
+	private Date firstReleaseDate;
+	private int price;
 	private String type;
+	
+	
+	public Car(){}
 	
 	public Car(String brand, String color, String type) {
 		super();
@@ -12,16 +20,32 @@ public class Car {
 		this.type = type;
 	}
 	
-	//public Car(){}
-	
 	public String getBrand() {
 		return brand;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
+	public Date getFirstReleaseDate() {
+		return firstReleaseDate;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
 	public String getType() {
 		return type;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
 	}
 	public void setBrand(String brand) {
 		this.brand = brand;
@@ -29,13 +53,20 @@ public class Car {
 	public void setColor(String color) {
 		this.color = color;
 	}
+	public void setFirstReleaseDate(Date firstReleaseDate) {
+		this.firstReleaseDate = firstReleaseDate;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
 	
 	public void setType(String type) {
 		this.type = type;
 	}
 	@Override
 	public String toString() {
-		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + "]";
+		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + ", price=" + price + ", available="
+				+ available + ", firstReleaseDate=" + firstReleaseDate + "]";
 	}
 	
 	
